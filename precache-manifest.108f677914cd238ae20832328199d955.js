331acd1f8f9f5a9a2be015c00e34bb79934732fa
danielpriestley <dannypriestley3@gmail.com>
2020-08-26 13:18:22 +0100

    Updates

---
+++ precache-manifest.108f677914cd238ae20832328199d955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc18e68855ea2395eba965ee73c5aef",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "027437a241f4c6151fb6",
+    "url": "/react-tutorial/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ad7263fcb315948cdd40",
+    "url": "/react-tutorial/static/js/2.a4f9b25e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.a4f9b25e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "027437a241f4c6151fb6",
+    "url": "/react-tutorial/static/js/main.cc3b2b85.chunk.js"
+  },
+  {
+    "revision": "bc4a0c2ef4090a646fb6",
+    "url": "/react-tutorial/static/js/runtime-main.a6a3af57.js"
+  }
+]);
